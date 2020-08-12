60730091f423154cc9c7a0b5c1fb289eed506ca2
akulahemasanthoshidaughterofmurty <46022945+akulahemasanthoshidaughterofmurty@users.noreply.github.com>
2020-08-12 11:38:43 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Area
+{
+	public static void main(String[] args)
+	{
+		Scanner o=new Scanner(System.in);
+		//object creation or instantiation
+		System.out.println("enter radius");
+		float r=o.nextFloat();
+		float a=(3.14f*r*r);
+		System.out.println("area of circle="+a);
+	}
+}
+	
+	
