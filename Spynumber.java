c0f16737f165fa08affcf93ef2ede61ce575f8ba
akulahemasanthoshidaughterofmurty <46022945+akulahemasanthoshidaughterofmurty@users.noreply.github.com>
2020-09-04 17:49:22 +0530

    Update Spynumber.java

--- Spynumber.java
+++ Spynumber.java
@@ -5,16 +5,16 @@ class Spynumber
 	{
 		Scanner s=new Scanner(System.in);
 		int n=s.nextInt();
-		int sum=0,pro=1;
+		int sum=0,product=1;
 		while(n!=0)
 		{
 			int rem=n%10;
 			sum=sum+rem;
-			pro=pro*rem;
+			product=product*rem;
 			n=n/10;
 		}
-		System.out.println(sum +" and  "+pro);
-		if(sum==pro)
+		System.out.println(sum +" and  "+product);
+		if(sum==product)
 			System.out.println("it is a spy number");
 		else
 			System.out.println("it is not a spy number");
