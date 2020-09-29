bda858fbb257f8537be3d5f51c4c4fbbf14ecd62
akulahemasanthoshidaughterofmurty <46022945+akulahemasanthoshidaughterofmurty@users.noreply.github.com>
2020-09-29 17:10:11 +0530

    Add files via upload

---
+++ pattern20.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class pattern20
+{
+  	public static void main(String args[])
+	{
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print(i+"");
+			}
+		
+		System.out.println();
+	}
+}
+}
---
+++ pattern22.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class pattern22
+{
+  	public static void main(String args[])
+	{
+		for(int i='E';i>='A';i++)
+		{
+			for(int j='A';j<=i;j--)
+			{
+				System.out.print(j+" ");
+			}
+		
+		System.out.println();
+	}
+}
+}
