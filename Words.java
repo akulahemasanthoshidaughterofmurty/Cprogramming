14ebd6179d7d3479dccc9ecc5f75b8ed972a6738
akulahemasanthoshidaughterofmurty <46022945+akulahemasanthoshidaughterofmurty@users.noreply.github.com>
2020-11-05 21:59:55 +0530

    Add files via upload

---
+++ Words.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Words{
+ public static void main(String args[])
+	{
+		String str="my name is hema";
+		String a[]=str.split(" ");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println(" ");
+		for(int i=a.length-1;i>=0;i--)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
