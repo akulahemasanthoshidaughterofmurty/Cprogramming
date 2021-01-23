0ff49d1b68a1a82bd72727d332964bfaf3380110
akulahemasanthoshidaughterofmurty <46022945+akulahemasanthoshidaughterofmurty@users.noreply.github.com>
2021-01-23 22:00:05 +0530

    Create multiplyingwithcontinousnumbers.java

---
+++ multiplyingwithcontinousnumbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Firsts
+{
+	public static void main(String args[])
+	{
+		int c=0;
+		int sum=0;
+		
+		for(int i=1;i<10;i++)
+		{
+			c=c*10+i;
+			System.out.println(c*8+i);
+		}
+	}
+}
